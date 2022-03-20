5b9804469eba54aab3c419c00fff1f05f1ef665b
YoisSilver <azharchd1@gmail.com>
2022-03-20 15:25:19 +0530

    first commit

---
+++ contracts/wallet.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+contract wallet {
+
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+    mapping(bytes32 => Token) public tokenMapping;
+    
+    bytes32[] public tokenList;
+
+    mapping(address => mapping(bytes32 => uint256))public balances;
+
+    function addToken(bytes32 ticker, address tokenAddress) external {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+}
